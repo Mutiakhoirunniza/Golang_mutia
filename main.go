95338334570628bb6677486a5252247160fe2228
Mutiakhrnniza2002 <diahmutiachoirunnisa@gmail.com>
2023-08-20 21:16:56 +0700

    melalukan perubahan code

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("unkris jaya")
-	fmt.Println(" jaya jaya ")
-	fmt.Println("pusgiwa")
-	fmt.Println("Ft")
+	for i := 1; i < 15; i++ {
+		fmt.Print(i)
+	}
 }
